d0074195bc13d06ba5913ae2bbc4b10baa0dca00
Zhesong-Home <zhesongwu@hotmail.com>
2020-04-13 15:56:51 -0500

    merge with Nathan's Template

---
+++ js/ui.js
@@ -0,0 +1,13 @@
+$(window).on('load', function() {
+    console.log("TESTing");
+    $('.loading-overlay, .loading-overlay-image-container').hide();
+});
+
+$(document).ready(function() {
+
+    $('.sidebar-left i').click(function() {
+        console.log("click");
+        $(this).parent().addClass("d-none").removeClass("d-flex");
+    });
+
+});
