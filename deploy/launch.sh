313b435af01acce3aeae68fc78e38f5baeaecd2f
root <root@wherecovid19.cigi.illinois.edu>
2020-06-03 11:50:50 -0500

    update deploy scripts

--- deploy/launch.sh
+++ deploy/launch.sh
@@ -1,4 +1,5 @@
 docker run \
+  --restart always \
   --cap-add=NET_ADMIN \
   --name covid19 \
   -p 443:443 \
@@ -7,6 +8,6 @@ docker run \
   -e EMAIL=zhiyul@illinois.edu \
   -e URL=wherecovid19.cigi.illinois.edu \
   -v nginx_volume:/config \
-  --mount type=bind,src=/var/covid19_project/nginx.conf,dst=/config/nginx/site-confs/default \
+  --mount type=bind,src=/var/covid19_project/wherecovid19_webapp/deploy/nginx.conf,dst=/config/nginx/site-confs/default \
   --mount type=bind,src=/var/covid19_project/wherecovid19_webapp,dst=/var/www/covid19_app \
   linuxserver/letsencrypt
