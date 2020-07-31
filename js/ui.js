1caa7377489bd61de671950afed3e3944b6c5175
9bombs <b_nj18@live.com>
2020-07-31 18:44:24 -0500

    Test loader

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,12 @@
-$(window).on('load', function() {
-    // initialize our loader overlay
-    loader.initialize();
-    window.setTimeout(hide_loader, 500);
+$(window).on("load", function () {
+  // initialize our loader overlay
+  loader.initialize();
+  window.addEventListener("load", fadeEffect);
 });
 
+$("#map").on("load", hide_loader);
+
 function hide_loader() {
-    $('.loading-overlay, .loading-overlay-image-container').hide();
-    console.log("loaded");
+  $(".loading-overlay, .loading-overlay-image-container").hide();
+  console.log("loaded");
 }
