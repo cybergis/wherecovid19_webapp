276a1dd6127f5476ff783e06b791b656e10da2d5
Ethan Liang <57604288+lymine1996@users.noreply.github.com>
2020-11-13 17:00:15 -0600

    Change data source of demograhpics

--- preprocessing/cronjob/preprocess_cron.sh
+++ preprocessing/cronjob/preprocess_cron.sh
@@ -118,8 +118,8 @@ download_files(){
 	wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv -O  ./us-states.csv
 	wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv -O ./us-counties.csv
 	#IDPH
-	wget -O ./illinois/idph_CountyDemos.json http://www.dph.illinois.gov/sitefiles/CountyDemos.json?nocache=1
-	wget -O ./illinois/idph_COVIDZip.json http://www.dph.illinois.gov/sitefiles/COVIDZip.json?nocache=1
+	wget -O ./illinois/idph_CountyDemos.json https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetCountyDemographics
+	wget -O ./illinois/idph_COVIDZip.json https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetZip
 	wget -O ./illinois/idph_COVIDHistoricalTestResults.json http://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1
 	#WHO
         wget -O ./global-covid19-who-gis.json https://covid19.who.int/page-data/index/page-data.json
