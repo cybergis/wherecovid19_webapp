2b6c0dd269c269956dfef050accd292711b2540b
Yong Liang <lymine1996@163.com>
2020-12-15 14:45:34 -0600

    Add parallel coordinates

--- js/leaflet/leaflet.js
+++ js/leaflet/leaflet.js
@@ -1705,6 +1705,110 @@ var updateChart = function (graphic) {
 
 }
 
+///////////////////////////////// Plot Parallel Coordinates ///////////////////////////////
+
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 10, bottom: 10, left: 10},
+  width = 1000 - margin.left - margin.right,
+  height = 200 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#parallelCoords")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+d3.csv("https://raw.githubusercontent.com/luyuliu/COVID19-Dashboard/master/frontend/data/us-counties-attributes.csv", function(data) {
+
+  // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
+  var cols = ["TOT_POP","TOT_HH","PCT_CHLDN","PCT_YOUTH","PCT_AD","PCT_SR","PCT_WHT","PCT_NWHT","MED_HH_INC","PCT_BLW_POV_RT"];
+  dimensions = d3.keys(data[0]).filter(function(d) { return cols.includes(d) })
+  
+  // Reset axis names
+  var keyDict = {"TOT_POP":"Population","TOT_HH":"Household","PCT_CHLDN":"Children(%)","PCT_YOUTH":"Youth(%)","PCT_AD":"Adult(%)",
+  "PCT_SR":"Senior(%)","PCT_WHT":"White(%)","PCT_NWHT":"Non-white(%)","MED_HH_INC":"Median Income","PCT_BLW_POV_RT":"Poverty(%)"};  
+
+  // For each dimension, I build a linear scale. I store all in a y object
+  var y = {}
+  for (i in dimensions) {
+    name = dimensions[i]
+    y[name] = d3.scaleLinear()
+      .domain( d3.extent(data, function(d) { return +d[name]; }) )
+      .range([height, 0])
+  }
+
+  // Build the X scale -> it find the best position for each Y axis
+  x = d3.scalePoint()
+    .range([0, width])
+    .padding(1)
+    .domain(dimensions);
+
+  // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+  function path(d) {
+      return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+  }
+
+  // Highlight the specie that is hovered
+  var highlight = function(d){
+
+    selected_county = "FIPS" + d.countyFIPS.toString()
+
+    // first every group turns grey
+    d3.selectAll(".line")
+      .transition().duration(200)
+      .style("stroke", "lightgrey")
+      .style("opacity", "0.2")
+    // Second the hovered specie takes its color
+    d3.selectAll("." + selected_county)
+      .transition().duration(200)
+      .style("stroke", "#69b3a2")
+      .style("opacity", "1")
+  }
+
+  // Unhighlight
+  var doNotHighlight = function(d){
+    d3.selectAll(".line")
+      .transition().duration(200).delay(1000)
+      .style("stroke", function(d){ return("#69b3a2")} )
+      .style("opacity", "1")
+  }
+
+  // Draw the lines
+  svg
+    .selectAll("myPath")
+    .data(data)
+    .enter().append("path")
+    .attr("class", function (d) { return "line " + "FIPS" + d.countyFIPS.toString() } ) // 2 class for each line: 'line' and the group name
+    .attr("d",  path)
+    .style("fill", "none")
+    .style("stroke", "#69b3a2")
+    .style("opacity", 0.5)
+    .on("mouseover", highlight)
+    .on("mouseleave", doNotHighlight)
+
+  // Draw the axis:
+  svg.selectAll("myAxis")
+    // For each dimension of the dataset I add a 'g' element:
+    .data(dimensions).enter()
+    .append("g")
+    .attr("class", "axis")
+    // I translate this element to its right position on the x axis
+    .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+    // And I build the axis with the call function
+    .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+    // Add axis title
+    .append("text")
+      .style("text-anchor", "middle")
+      .attr("y", -9)
+      .text(function(d) { return keyDict[d]; })
+      .style("fill", "white")
+
+})
+
 /////////////////////////////////////// Create Popup //////////////////////////////////////
 
 var createPopup = function (_feature, _layer) {
